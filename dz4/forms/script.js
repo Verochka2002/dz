ec9d2d04eaf6dc274b585138a088dca93cf0ff9f
Verochka2002 <87915632+Verochka2002@users.noreply.github.com>
2021-10-15 16:46:35 +0300

    Add files via upload

---
+++ dz4/forms/script.js
@@ -0,0 +1,10 @@
+function validateComments(input) {
+    if (input.value.length < 20) {
+        input.setCustomValidity("Дайте более развернутый ответ.");
+    }
+    else {
+        // Длина комментария отвечает требованию, 
+        // поэтому очищаем сообщение об ошибке
+        input.setCustomValidity("");
+    }
+}
